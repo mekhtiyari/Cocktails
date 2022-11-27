6668f752f6297e18e15d353ceed21adc99d70fac
mekhtiyari <m.ekhtiyari1992@gmail.com>
2022-11-28 00:47:38 +0330

    project completed

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Ad dolor irure enim aute anim consectetur. Exercitation commodo minim
+        anim ullamco fugiat et. Amet do eiusmod deserunt eu nulla. Consequat
+        nostrud ipsum culpa voluptate nulla minim tempor dolore aute. Quis
+        veniam ipsum eiusmod esse ex dolore ea quis commodo adipisicing ea Lorem
+        commodo in. Duis sit aute non eiusmod culpa in culpa cupidatat. Elit
+        quis esse ipsum occaecat dolore voluptate incididunt sit non aute
+        occaecat ea fugiat nostrud.
+      </p>
+    </section>
+  );
+};
+
+export default About;
